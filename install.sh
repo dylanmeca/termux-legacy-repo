2efc1e1593538da633e94bc7044f9be7c46cc708
Dylan Meca <55631040+dylanmeca@users.noreply.github.com>
2022-01-23 22:12:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "[*] Installing dependencies..."
 cd $HOME
-pkg update -y
+pkg update -y && pkg upgrade -y
 pkg install wget -y
 pkg install gnupg -y
 echo "[*] Installing apt repository..."
+echo "deb https://dylanmeca.github.io/termux-legacy-repo termux extras" > $PREFIX/etc/apt/sources.list.d/termuxlegacy.list
 wget -c --quiet --show-progress https://raw.githubusercontent.com/dylanmeca/termux-legacy-repo/main/termuxlegacy.key
 chmod +x termuxlegacy.key
 apt-key add termuxlegacy.key
-echo "deb https://dylanmeca.github.io/termux-legacy-repo termux extras" > $PREFIX/etc/apt/sources.list.d/termuxlegacy.list
 echo "[*] Updating packages..."
 pkg update -y
 echo "[*] Installation complete"
